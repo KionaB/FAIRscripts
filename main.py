3a45a6ca6fcf9c02ddfab2ddb1081ccd93ff6d73
Kiona Bijker <k.bijker@student.utwente.nl>
2025-10-14 15:51:57 +0200

    Added RDF minor code fixes

--- main.py
+++ main.py
@@ -4,33 +4,35 @@ import pandas as pd
 df_original = pd.read_csv('train-lending-club-columns-used.csv')
 df_edit = df_original.copy()
 
-#Check that id is unique
+# Check that id is unique
 ids = df_edit['id'].unique()
-if not(len(ids) == len(df_edit['id'])):
+if not (len(ids) == len(df_edit['id'])):
     print("id column is not unique")
 else:
     print("id column is unique")
 
+
 def find_unique_vals(df):
-    #Determine unique values for (for enumeration later):
+    # Determine unique values for (for enumeration later):
     unique_vals = dict()
-    cols =['sub_grade','term','home_ownership','application_type','verification_status','addr_state','loan_status']
+    cols = ['sub_grade', 'term', 'home_ownership', 'application_type', 'verification_status', 'addr_state', 'loan_status']
     for col in cols:
-        uni_vals = df_edit[col].unique()
+        uni_vals = df[col].unique()
         uni_vals.sort()
         unique_vals[col] = uni_vals.copy()
     return unique_vals
 
-#Sample 1000 randomly
-df_sample = df_edit.sample(1000,replace=False,random_state=42,ignore_index=True)
 
-#Check that all options are represented
+# Sample 1000 randomly
+df_sample = df_edit.sample(1000, replace=False, random_state=42, ignore_index=True)
+
+# Check that all options are represented
 unique_ori = find_unique_vals(df_original.copy())
-unique_sample =find_unique_vals(df_sample.copy())
+unique_sample = find_unique_vals(df_sample.copy())
 for key in unique_ori.keys():
-    print(key +"_original: " + str(len(unique_ori[key])))
+    print(key + "_original: " + str(len(unique_ori[key])))
     print(key + "_sample: " + str(len(unique_sample[key])))
 
 print(unique_ori)
-#Write DataFrame to CSV
+# Write DataFrame to CSV
 # df_sample.to_csv('train-lending-club-filtered1000.csv', index=False)
